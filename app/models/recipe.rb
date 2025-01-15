aed50c7f4f50531c13f037d5c21509eeb3b6182b
amie retallick <amie.retallick@gmail.com>
2025-01-15 17:26:18 +0000

    APIs completed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
   has_many :categories, through: :bookmarks
+  has_one_attached :image
 
   validates :description, presence: true
   validates :name, presence: true, uniqueness: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,33 +8,56 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+p "Cleaning the database...."
+Bookmark.destroy_all
 Recipe.destroy_all
+p "Complete. Now loading recipes..."
 
-Recipe.create!([
-  {
-    name: 'Spaghetti Bolognese',
-    description: 'A classic Italian pasta dish with a rich, savory meat sauce.',
-    rating: 4,
-    image_url: 'https://plus.unsplash.com/premium_photo-1677000666741-17c3c57139a2?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: 'Chicken Curry',
-    description: 'A spicy and aromatic curry made with tender chicken pieces.',
-    rating: 2,
-    image_url: 'https://plus.unsplash.com/premium_photo-1723708871094-2c02cf5f5394?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: 'Vegetable Stir Fry',
-    description: 'A quick stir fry with assorted fresh vegetables and soy sauce.',
-    rating: 5,
-    image_url: 'https://plus.unsplash.com/premium_photo-1664472637341-3ec829d1f4df?q=80&w=2825&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  },
-  {
-    name: 'Beef Tacos',
-    description: 'Tacos with seasoned ground beef, fresh toppings, and a soft corn shell.',
-    rating: 5,
-    image_url: 'https://images.unsplash.com/photo-1504544750208-dc0358e63f7f?q=80&w=2834&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-  }
-])
+require "open-uri"
+require "json"
 
-puts "4 recipes with image URLs created!"
+categories = ["Pasta", "Dessert", "Breakfast", "Vegetarian"]
+recipes_by_category = {}
+
+categories.each do |category|
+  url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=#{category}"
+  recipes_serialised = URI.open(url).read
+  recipes = JSON.parse(recipes_serialised)
+  recipes_by_category[category] = recipes
+  five_recipes = recipes["meals"].first(5)
+  # p five_recipes
+  five_recipes.each do |recipe|
+    url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=#{recipe["idMeal"]}"
+    recipes_serialised = URI.open(url).read
+    recipes_array_hash = JSON.parse(recipes_serialised)
+    recipe_hash = recipes_array_hash["meals"][0]
+      Recipe.create!(
+      {
+        name: recipe_hash["strMeal"],
+        description: recipe_hash["strArea"],
+        rating: 4,
+        image_url: recipe_hash["strMealThumb"]
+      }
+    )
+  end
+end
+
+p "Complete. Recipes loaded."
+
+# recipes_by_category.each do |category, data|
+#   # puts "Category: #{category}"
+#   data["meals"].each do |meal|
+#     puts "#{meal["idMeal"]}"
+#   end
+# end
+
+# Recipe.destroy_all
+
+# Recipe.create!([
+#   {
+#     name: recipe["strMeal"],
+#     description: 'A classic Italian pasta dish with a rich, savory meat sauce.',
+#     rating: 4,
+#     image_url: 'https://plus.unsplash.com/premium_photo-1677000666741-17c3c57139a2?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+#   }
+# ])
